7c787883c20d7126418a884721ba6f1552182ef3
Anshi LeFlay <necrofallen@gmail.com>
2023-02-12 00:49:28 +0400

    prettier services

--- src/services/socketMiddleware.ts
+++ src/services/socketMiddleware.ts
@@ -1,56 +1,55 @@
-import type { Middleware, MiddlewareAPI } from 'redux';
+import type { Middleware, MiddlewareAPI } from "redux";
 
-import type { AppThunk, RootState } from './types';
+import type { AppThunk, RootState } from "./types";
 
 export const socketMiddleware = (wsUrl: string): Middleware => {
     return ((store: MiddlewareAPI<AppThunk, RootState>) => {
         let socket: WebSocket | null = null;
 
-    return next => (action: any) => {
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        const { dispatch, getState } = store;
-        const { type, payload, url } = action;
+        return (next) => (action: any) => {
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            const { dispatch, getState } = store;
+            const { type, payload, url } = action;
 
-        if (type === 'WS_CONNECTION_START') {
-            socket = new WebSocket( `${ wsUrl }${ url }` );
-        }
-
-        if (socket) {
-            // функция, которая вызывается при открытии сокета
-            socket.onopen = event => {
-                dispatch({ type: 'WS_CONNECTION_SUCCESS', payload: event });
-            };
-
-            // функция, которая вызывается при ошибке соединения
-            socket.onerror = event => {
-                dispatch({ type: 'WS_CONNECTION_ERROR', payload: event });
-            };
-
-            // функция, которая вызывается при получения события от сервера
-            socket.onmessage = event => {
-                const { data } = event;
-                dispatch({ type: 'WS_GET_MESSAGE', payload: data });
-                //console.log( data );
-            };
-
-            // функция, которая вызывается при закрытии соединения
-            socket.onclose = event => {
-                dispatch({ type: 'WS_CONNECTION_CLOSED', payload: event });
-            };
-
-            if (type === 'WS_SEND_MESSAGE') {
-                const message = payload;
-                // функция для отправки сообщения на сервер
-                socket.send(JSON.stringify(message));
+            if (type === "WS_CONNECTION_START") {
+                socket = new WebSocket(`${wsUrl}${url}`);
             }
-            
-            if (type === 'WS_CONNECTION_END') {
-                socket.close();
-            }
-            
-        }
 
-        next(action);
-    };
+            if (socket) {
+                // функция, которая вызывается при открытии сокета
+                socket.onopen = (event) => {
+                    dispatch({ type: "WS_CONNECTION_SUCCESS", payload: event });
+                };
+
+                // функция, которая вызывается при ошибке соединения
+                socket.onerror = (event) => {
+                    dispatch({ type: "WS_CONNECTION_ERROR", payload: event });
+                };
+
+                // функция, которая вызывается при получения события от сервера
+                socket.onmessage = (event) => {
+                    const { data } = event;
+                    dispatch({ type: "WS_GET_MESSAGE", payload: data });
+                    //console.log( data );
+                };
+
+                // функция, которая вызывается при закрытии соединения
+                socket.onclose = (event) => {
+                    dispatch({ type: "WS_CONNECTION_CLOSED", payload: event });
+                };
+
+                if (type === "WS_SEND_MESSAGE") {
+                    const message = payload;
+                    // функция для отправки сообщения на сервер
+                    socket.send(JSON.stringify(message));
+                }
+
+                if (type === "WS_CONNECTION_END") {
+                    socket.close();
+                }
+            }
+
+            next(action);
+        };
     }) as Middleware;
 };
--- src/services/store.ts
+++ src/services/store.ts
@@ -1,19 +1,22 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 
-import { socketMiddleware } from './socketMiddleware';
-import { rootReducer } from './reducers';
+import { socketMiddleware } from "./socketMiddleware";
+import { rootReducer } from "./reducers";
 
-const wsUrl = 'wss://norma.nomoreparties.space/orders';
+const wsUrl = "wss://norma.nomoreparties.space/orders";
 
 declare global {
     interface Window {
-      __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+        __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
     }
 }
-  
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const enhancer = composeEnhancers( applyMiddleware( thunk ), applyMiddleware( socketMiddleware( wsUrl ) ) );
+const enhancer = composeEnhancers(
+    applyMiddleware(thunk),
+    applyMiddleware(socketMiddleware(wsUrl))
+);
 
-export const store = createStore( rootReducer, enhancer );
+export const store = createStore(rootReducer, enhancer);
